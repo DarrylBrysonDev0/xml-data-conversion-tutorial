e93f718db7bc47d7805d267eb142000943b4ea23
DarrylBrysonDev0 <62783148+DarrylBrysonDev0@users.noreply.github.com>
2021-03-25 16:48:13 -0400

    Update batch_xml_convertion.py

--- app/batch_xml_convertion.py
+++ app/batch_xml_convertion.py
@@ -6,11 +6,41 @@
 import sys
 import os
 
+# XML Structure Class
+## class represents the structure and handler methods
+class Xml_Parser:
+    def __init__(self):
+        self.ResultAr = []
+        return
+
 # Functions
-def convert_local():
+def convert_local_dir(source_dir: str, target_dir: str) -> None:
+    # Get list of files to convert
+    # For each file
+    data = convert_file_to_obj(source_dir)
+    ## Validate structure
+    ## Apply class
+    ## Filter field
     return
-def convert_remote():
+def convert_remote_dir(source_dir: str, target_dir: str, conn_dict: dict) -> None:
+    # Connect to sftp server
+    # Get list of files to convert
+    # For each file
+    # Download file locally
     return
+def convert_file_to_obj(conv_f):
+    try:
+        with open(conv_f) as f:
+            data = json.load(f)
+            producer.send(topic,value=data)
+            # print(str(msg_file))
+            msg_cnt+=1
+    except Exception as err:
+        print()
+        print("An error occurred while reading file {0}".format(fo))
+        print(str(err))
+        traceback.print_tb(err.__traceback__)
+    return res
 def set_env_param(paramName,defaultStr):
     param = os.getenv(paramName)
     res = defaultStr if not param else param
@@ -21,8 +51,25 @@ if __name__ == '__main__':
         # Get parameters from environment 
         local_src_dir = set_env_param('LOCAL_SOURCE_DIR','')
         local_opt_dir = set_env_param('LOCAL_OUTPUT_DIR','')
+
+        # SFTP connection details 
+        sftp_address = set_env_param('SFTP_HOST_ADDRESS','')
+        sftp_usr = set_env_param('SFTP_USER','')
+        sftp_pwd = set_env_param('SFTP_PASSWORD','')
+        sftp_conn_dict = {
+            'host'=sftp_address
+            ,'usr'=sftp_usr
+            ,'pwd'=sftp_pwd
+        }
+
         remote_src_dir = set_env_param('REMOTE_SOURCE_DIR','')
         remote_opt_dir = set_env_param('REMOTE_OUTPUT_DIR','')
+
+        # Convert local directory of xml files
+        convert_local(local_src_dir,local_opt_dir)
+
+        # Convert remote directory of xml files
+        convert_remote(remote_src_dir,remote_opt_dir, sftp_conn_dict)
         print(" [*] Starting main")
         main()
         print(' [*] Closing application')
